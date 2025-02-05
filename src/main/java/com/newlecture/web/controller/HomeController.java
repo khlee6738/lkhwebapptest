046b63ee643dbe4f65bd8a541292e2e76f0c8284
LeeKyuHo <khlee6738@naver.com>
2025-02-05 21:20:47 +0900

    first commit

---
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.newlecture.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/index")
+	public String adsf() {
+		return "Hello Spring Boot";
+	}
+
+}
