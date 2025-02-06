5ebf71d269cf31db41168416efa89857e57812c3
YM <YM@DESKTOP-OGPFS2Q>
2025-02-06 13:40:12 +0900

    war용

--- src/main/java/com/newlecture/web/SpringWebApplication.java
+++ src/main/java/com/newlecture/web/SpringWebApplication.java
@@ -2,12 +2,21 @@ package com.newlecture.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringWebApplication {
+@ComponentScan
+public class SpringWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringWebApplication.class);
+    }
 
 }
